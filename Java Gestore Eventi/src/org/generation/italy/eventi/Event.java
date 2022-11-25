015d3d9a961f15bd9cf006372d45fcc2c5250681
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-11-25 10:10:48 +0100

    feat(Event): add attributes, constructor, getters and setters

--- Java Gestore Eventi/src/org/generation/italy/eventi/Event.java
+++ Java Gestore Eventi/src/org/generation/italy/eventi/Event.java
@@ -1,5 +1,49 @@
 package org.generation.italy.eventi;
 
-public class Event {
+import java.time.LocalDate;
 
+public class Event {
+	
+	private String title;
+	private LocalDate date;
+	private int totalSeatNumber;
+	private int reservedSeatNumber;
+	
+	Event(String title, LocalDate date, int totalSeatNumber) throws Exception{
+		
+		setTitle(title);
+		setDate(date);
+		
+		if(totalSeatNumber <= 0) {
+			throw new Exception("The total number of seats must be greater than zero.");
+		}
+		
+		this.totalSeatNumber = totalSeatNumber;
+		this.reservedSeatNumber = 0;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public int getTotalSeatNumber() {
+		return totalSeatNumber;
+	}
+
+	public int getReservedSeatNumber() {
+		return reservedSeatNumber;
+	}
+	
 }
