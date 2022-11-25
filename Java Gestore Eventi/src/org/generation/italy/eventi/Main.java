9f4674f485fac62f51742daa22d00bacb0f57e9a
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-11-25 17:33:08 +0100

    feat(Main): testing ProgrammEvents class
    apply the methods present in the class.
    - Milestone3 completed

--- Java Gestore Eventi/src/org/generation/italy/eventi/Main.java
+++ Java Gestore Eventi/src/org/generation/italy/eventi/Main.java
@@ -1,100 +1,137 @@
 package org.generation.italy.eventi;
 
 import java.time.LocalDate;
-import java.util.Scanner;
+import java.util.List;
+//import java.util.Scanner; -> active for MILESTONE2
 
 public class Main {
 
 	public static void main(String[] args) {
+
+//		MILESTONE2
+//		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.print("Enter event title: ");
+//		String title = sc.next();
+//		
+//		System.out.println("--------------------");
+//		
+//		System.out.print("Enter date of the event [aaaa-mm-dd]: ");
+//		String dateStr = sc.next();
+//		LocalDate date = LocalDate.parse(dateStr);
+//		
+//		System.out.println("--------------------");
+//		
+//		System.out.print("Enter total seat number: ");
+//		int totalSeatNumber = sc.nextInt();
+//		
+//		System.out.println("--------------------\n");
+//		
+//		Event event1 = null;
+//		try {
+//			event1 = new Event(title, date, totalSeatNumber);
+//		} catch (Exception e) {
+//			System.err.println(e.getMessage());
+//		} finally {
+//			System.out.println(event1 == null ? "The event could not be created" : event1);
+//		}
+//		
+//		System.out.println("\n--------------------\n");
+//		
+//		
+//		if(event1 != null) {
+//			boolean exit = false;
+//			while(!exit) {
+//			
+//				System.out.println("Choose an option to perform the operation" 
+//					+ "\n1 - reserve"
+//					+ "\n2 - cancel"
+//					+ "\n3 - exit");
+//				
+//				String userChoice = sc.next();
+//				
+//				if (userChoice.equals("1")) {
+//					System.out.println("How many reservations do you want to make? ");
+//					int numberReservation = sc.nextInt();
+//		
+//					for (int i = 0; i < numberReservation; i++) {
+//						try {
+//							event1.reserve();
+//						} catch (Exception e) {
+//							System.err.println(e.getMessage());;
+//						}
+//					}
+//		
+//					System.out.println( "\nEvent title: " + event1.getTitle()
+//										+"\nReserved seats: " + event1.getReservedSeatNumber() 
+//										+ "\nAvailable seats: " + (event1.getTotalSeatNumber() - event1.getReservedSeatNumber()) + "\n");
+//				}
+//				
+//				if(userChoice.equals("2")) {
+//					System.out.println("How many cancellations do you want to make? ");
+//					int numberCancellation = sc.nextInt();
+//		
+//					for (int i = 0; i < numberCancellation; i++) {
+//						try {
+//							event1.cancel();
+//						} catch (Exception e) {
+//							System.err.println(e.getMessage());;
+//						}
+//					}
+//		
+//					System.out.println("\nEvent title: " + event1.getTitle()
+//										+"\nReserved seats: " + event1.getReservedSeatNumber() 
+//										+ "\nAvailable seats: " + (event1.getTotalSeatNumber() - event1.getReservedSeatNumber()) + "\n");
+//				}
+//				
+//				if (!userChoice.equals("1") && !userChoice.equals("2") || userChoice.equals("3")) {
+//					System.out.println("\nYou have exited from the system, here are the results");
+//					
+//					System.out.println("\nEvent title: " + event1.getTitle()
+//										+"\nReserved seats: " + event1.getReservedSeatNumber() 
+//										+ "\nAvailable seats: " + (event1.getTotalSeatNumber() - event1.getReservedSeatNumber()));
+//					exit = true;
+//				}
+//				
+//			}
+//		}
+//		
+//		sc.close();
 		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Enter event title: ");
-		String title = sc.next();
-		
-		System.out.println("--------------------");
-		
-		System.out.print("Enter date of the event [aaaa-mm-dd]: ");
-		String dateStr = sc.next();
-		LocalDate date = LocalDate.parse(dateStr);
-		
-		System.out.println("--------------------");
-		
-		System.out.print("Enter total seat number: ");
-		int totalSeatNumber = sc.nextInt();
-		
-		System.out.println("--------------------\n");
-		
+//		MILESTONE3
 		Event event1 = null;
+		Event event2 = null;
+		Event event3 = null;
+		Event event4 = null;
+		Event event5 = null;
+		
 		try {
-			event1 = new Event(title, date, totalSeatNumber);
+			event1 = new Event("Sagra", LocalDate.parse("2023-06-23"), 50 );
+			event2 = new Event("Fiera", LocalDate.parse("2023-11-11"), 70 );
+			event3 = new Event("Cinema", LocalDate.parse("2023-05-15"), 100 );
+			event4 = new Event("Concerto", LocalDate.parse("2023-04-02"), 200 );
+			event5 = new Event("Mondiali", LocalDate.parse("2023-11-11"), 5000 );
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
-		} finally {
-			System.out.println(event1 == null ? "The event could not be created" : event1);
 		}
 		
-		System.out.println("\n--------------------\n");
+		ProgrammEvents pe = new ProgrammEvents("TOP EVENTS");
+		pe.addEvent(event1);
+		pe.addEvent(event2);
+		pe.addEvent(event3);
+		pe.addEvent(event4);
+		pe.addEvent(event5);
+		System.out.println(pe);
 		
+		List<Event> eventOf11November = pe.getEventsByDate(LocalDate.parse("2023-11-11"));
 		
-		if(event1 != null) {
-			boolean exit = false;
-			while(!exit) {
-			
-				System.out.println("Choose an option to perform the operation" 
-					+ "\n1 - reserve"
-					+ "\n2 - cancel"
-					+ "\n3 - exit");
-				
-				String userChoice = sc.next();
-				
-				if (userChoice.equals("1")) {
-					System.out.println("How many reservations do you want to make? ");
-					int numberReservation = sc.nextInt();
+		System.out.println("Events scheduled for November 11th: " + eventOf11November);
 		
-					for (int i = 0; i < numberReservation; i++) {
-						try {
-							event1.reserve();
-						} catch (Exception e) {
-							System.err.println(e.getMessage());;
-						}
-					}
+		System.out.println("Number of events in the program: " + pe.getNumberEvents());
 		
-					System.out.println( "\nEvent title: " + event1.getTitle()
-										+"\nReserved seats: " + event1.getReservedSeatNumber() 
-										+ "\nAvailable seats: " + (event1.getTotalSeatNumber() - event1.getReservedSeatNumber()) + "\n");
-				}
-				
-				if(userChoice.equals("2")) {
-					System.out.println("How many cancellations do you want to make? ");
-					int numberCancellation = sc.nextInt();
-		
-					for (int i = 0; i < numberCancellation; i++) {
-						try {
-							event1.cancel();
-						} catch (Exception e) {
-							System.err.println(e.getMessage());;
-						}
-					}
-		
-					System.out.println("\nEvent title: " + event1.getTitle()
-										+"\nReserved seats: " + event1.getReservedSeatNumber() 
-										+ "\nAvailable seats: " + (event1.getTotalSeatNumber() - event1.getReservedSeatNumber()) + "\n");
-				}
-				
-				if (!userChoice.equals("1") && !userChoice.equals("2") || userChoice.equals("3")) {
-					System.out.println("\nYou have exited from the system, here are the results");
-					
-					System.out.println("\nEvent title: " + event1.getTitle()
-										+"\nReserved seats: " + event1.getReservedSeatNumber() 
-										+ "\nAvailable seats: " + (event1.getTotalSeatNumber() - event1.getReservedSeatNumber()));
-					exit = true;
-				}
-				
-			}
-		}
-		
-		sc.close();
+		pe.clearEvents();
+		System.out.println("Number of events present in the program after clear: " + pe.getNumberEvents());
 	}
 
 }
