ecdac3f5b93747d9f5e7c3d3147cda84aeee2f75
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-11-25 17:30:01 +0100

    feat(ProgrammEvents): add attributes and methods
    constructor value the title, passed as a parameter, and initialize the list of events as a new ArrayList
    
    added methods:
    
    - a method that adds an Event to the list, passed as a parameter
    
    - a method that returns a list with all the events present on a certain date
    
    - a method that returns how many events are present in the program
    
    - a method that empties the list of events
    
    - a method that returns a string showing the title of the program and all the events sorted by date

--- Java Gestore Eventi/src/org/generation/italy/eventi/ProgrammEvents.java
+++ Java Gestore Eventi/src/org/generation/italy/eventi/ProgrammEvents.java
@@ -1,5 +1,63 @@
 package org.generation.italy.eventi;
 
-public class ProgrammEvents {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class ProgrammEvents {
+	private String title;
+	private List<Event> events;
+
+
+	public ProgrammEvents(String title2) {
+		this.title = title2;
+		this.events = new ArrayList<>();
+	}
+
+	public void addEvent(Event e) {
+		events.add(e);
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public List<Event> getEventsByDate(LocalDate date) {
+		List<Event> eventsByDate = new ArrayList<>();
+		for (Event e : events) {
+			if (e.getDate().equals(date)) {
+				eventsByDate.add(e);
+			}
+		}
+		return eventsByDate;
+	}
+	
+	public int getNumberEvents() {
+		return events.size();
+	}
+	
+	public void clearEvents() {
+		events.clear();
+	}
+	
+	public String getFormattedList() {
+		String formattedList = getTitle() + "\n";
+		
+		sortEvents();
+		
+		for (Event e : events) {
+			formattedList += e.getFormattedDate() + " - " + e.getTitle() + "\n";
+		}
+		
+		return formattedList;
+	}
+	
+	private void sortEvents() {
+		events.sort((e1, e2) -> e1.getDate().compareTo(e2.getDate()));
+	}
+
+	@Override
+	public String toString() {
+		return getFormattedList();
+	}
 }
