31cc6095d431a664f3a76ea606fc8a5239fd98c6
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-11-25 12:20:18 +0100

    feat(Main): add prompt for the user to enter a new event
    verified the creation with "try catch".
    In case of error a message is returned, otherwise the event is printed

--- Java Gestore Eventi/src/org/generation/italy/eventi/Main.java
+++ Java Gestore Eventi/src/org/generation/italy/eventi/Main.java
@@ -1,10 +1,38 @@
 package org.generation.italy.eventi;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("Enter event title: ");
+		String title = sc.next();
+		
+		System.out.println("--------------------");
+		
+		System.out.print("Enter date of the event [aaaa-mm-dd]: ");
+		String dateStr = sc.next();
+		LocalDate date = LocalDate.parse(dateStr);
+		
+		System.out.println("--------------------");
+		
+		System.out.print("Enter total seat number: ");
+		int totalSeatNumber = sc.nextInt();
+		
+		System.out.println("--------------------\n");
+		
+		Event event1 = null;
+		try {
+			event1 = new Event(title, date, totalSeatNumber);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		} finally {
+			System.out.println(event1 == null ? "The event could not be created" : event1);
+		}
 	}
 
 }
